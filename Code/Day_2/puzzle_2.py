4fd905f38d8218a2f68164efc62ed0919e1c919b
Martin Risøy <martiris@hiof.no>
2023-12-02 18:44:37 +0100

    this is the second day of advent of code with code that works.

--- Code/Day_2/puzzle_2.py
+++ Code/Day_2/puzzle_2.py
@@ -15,14 +15,11 @@ for i in range(len(games)):
             dice_values = list(pull[dice].split(" "))
             if game_pulls[dice_values[1]] < int(dice_values[0]):
                 game_pulls[dice_values[1]] = int(dice_values[0])
-    print(dice_pulls)
-    print(game_pulls)
     lowest_values.append([game_pulls["red"], game_pulls["green"], game_pulls["blue"]])
+
 sum_of_powers = 0
-print(len(lowest_values))
 for i in range(len(lowest_values)):
     power = lowest_values[i][0] * lowest_values[i][1] * lowest_values[i][2]
-    print(i + 1, lowest_values[i], power)
     sum_of_powers += power
 print(sum_of_powers)
 
